f1b73005ce5050bdaca1d7f0e01363f68efe947b
Martha Jaworski <martha.jaworski@gmail.com>
2013-04-14 15:21:37 -0700

    Created Hello World!

---
+++ src/martha/helloworld/HelloWorld.java
@@ -0,0 +1,14 @@
+package martha.helloworld;
+
+public class HelloWorld {
+
+	/**
+	 * Greet the world.
+	 * 
+	 * @param args Arguments to our program; ignored.
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
+}
